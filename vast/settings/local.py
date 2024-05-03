e37b1fee7ae42e2a919e8a215b2d9ae9a5c96a79
hassan0184 <mianhassanshahzad@gmail.com>
2024-05-03 11:33:16 +0500

    added cors settings

--- vast/settings/local.py
+++ vast/settings/local.py
@@ -1,9 +1,10 @@
 from .base import *
 
+
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.0:8000',
-    'http://44.210.118.15:8000/',
-    'http://localhost:3000/',
+    'http://44.210.118.15:8000',
+    'http://localhost:3000',
 ]
 AWS_S3_CUSTOM_DOMAIN = ""
 
